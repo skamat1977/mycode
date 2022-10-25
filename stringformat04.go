765131c2c8c973838e13bd4ae9c629d5e4e65bf4
skamat1977 <skamat1977@yahoo.com>
2022-10-25 01:29:45 +0000

    learning about go arrays

---
+++ stringformat04.go
@@ -0,0 +1,33 @@
+/* Alta3 Research | RZFeeser
+   CHALLENGE 01 - using fmt.Sprintf() to construct
+   https://example.org:6001/v2/snacks?req=snickers&quantity=1&size=king
+   
+   `uri` is `https://example.org:6001/v2/snacks?`
+   `r`   is `req=snickers`
+   `q`   is `quantity=1`
+   `s`   is `size=king`         */
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    // declare a const uri (this will never change)
+    const uri = "https://example.org:6001/v2/snacks?"
+
+    // declare our parameters
+    // this is a group declaration
+   // var r, q, s string = "req=snickers", "quantity=1", "size=king" 
+   type PirateTreasure struct {
+    r, q, s string            // r q and s are all "strings" {r q s}
+}
+    p := PirateTreasure{"req=snickers", "quantity=1", "size=king"}
+
+    // create the string, but don't render it yet
+    res := fmt.Sprintf("%s%s&%s&%s", uri, p)
+    fmt.Println(res) // finished URI
+}
+
